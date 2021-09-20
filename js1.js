a6d9241eaddc877a7e3c32cd6825de5b1d0d99e2
YunSeongLee22 <91048293+YunSeongLee22@users.noreply.github.com>
2021-09-21 00:03:49 +0900

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,226 @@
+
+       function menu1(){
+        var menu=["짜장면","마라탕","돈가스","김치찌개","회덮밥","초밥"
+        ,"피자","헴버거","파스타","치킨","굶기ㅋㅋ!","서브웨이","샐러드","칼국수"
+        ,"쌈밥","씨리얼","야채랑밥이랑비벼먹기","삼각김밥","라면","치킨","텐동"]
+        var menuNum= Math.floor(Math.random()*menu.length);
+ 
+        alert(menu[menuNum]);
+        }
+ 
+        function coffee(){
+        var menu1=["아메리카노","더치","카페라떼","카푸치노","카페모카"
+        ,"캬라멜카페모카","카페비엔나","카페콘파냐","아포카토","바닐라라떼"
+        ,"모라카떼","헤이즐넛라떼","연유라떼","초코쿠키프라페"]
+        var menuNum1= Math.floor(Math.random()*menu1.length);
+ 
+        alert(menu1[menuNum1]);
+        }
+ 
+        function luck(){
+            var lucky = Math.floor(Math.random()*100);
+ 
+            alert(lucky+  " //  주의!! 하루에 한 번만 하시오");
+            
+        }
+        function yt(){
+            location.href="https://www.youtube.com/?hl=KO"           
+        }
+        function lol(){
+            location.href="https://www.op.gg/"           
+        }
+        
+        function google(){
+            location.href="https://www.google.co.kr/"           
+        }
+ 
+        function naver(){
+            location.href="https://www.naver.com/"           
+        }
+ 
+        function chelsea(){
+            location.href="https://www.youtube.com/user/chelseafc"           
+        }
+        function sc(){
+            location.href="https://medea.tv/index.php?device=pc"           
+        }
+        function folder(){
+            location.href="C:/Users/user/Desktop/YS"           
+        }
+ 
+        function maple(){
+            location.href="C:/Users/user/Desktop/GameLauncher.exe"           
+        }
+ 
+        $(function(){
+         $(".h").hover
+         (function()
+            {
+             $(".a").css({"background" : "rgb(8,81,144)" });   
+            }, function() {
+             $(".a").css({"background" : "none"});   
+            }, 
+        );
+     });
+ 
+        $(function(){
+         $(".h").hover
+         (function()
+            {
+             $(".b").css({ "background" : "rgb(8,81,144)" , "color" : "yellow"});
+            }, function(){
+             $(".b").css({ "background" : "none","color" : "black"});
+            });
+        });
+ 
+        $(function(){
+         $(".h1").hover
+         (function()
+            {
+             $(".a1").css({"background" : "rgb(8,81,144)"});   
+            }, function() {
+             $(".a1").css({"background" : "none"});   
+            });
+        });
+ 
+        $(function(){
+         $(".h1").hover
+         (function()
+            {
+             $(".b1").css({ "background" : "rgb(8,81,144)" , "color" : "yellow"});
+            }, function(){
+             $(".b1").css({ "background" : "none","color" : "black"});
+            });
+        });
+        $(function(){
+         $(".h2").hover
+         (function()
+            {
+             $(".a2").css({"background" : "rgb(8,81,144)"});   
+            }, function() {
+             $(".a2").css({"background" : "none"});   
+            });
+        });
+        $(function(){
+         $(".h2").hover
+         (function()
+            {
+             $(".b2").css({ "background" : "rgb(8,81,144)" , "color" : "yellow"});
+            }, function(){
+             $(".b2").css({ "background" : "none","color" : "black"});
+            });
+        });
+        $(function(){
+         $(".h3").hover
+         (function()
+            {
+             $(".a3").css({"background" : "rgb(8,81,144)"});   
+            }, function() {
+             $(".a3").css({"background" : "none"});   
+            });
+        });
+        $(function(){
+         $(".h3").hover
+         (function()
+            {
+             $(".b3").css({ "background" : "rgb(8,81,144)" , "color" : "yellow"});
+            }, function(){
+             $(".b3").css({ "background" : "none","color" : "black"});
+            });
+        });
+        $(function(){
+         $(".h4").hover
+         (function()
+            {
+             $(".a4").css({"background" : "rgb(8,81,144)"});   
+            }, function() {
+             $(".a4").css({"background" : "none"});   
+            });
+        });
+        $(function(){
+         $(".h4").hover
+         (function()
+            {
+             $(".b4").css({ "background" : "rgb(8,81,144)" , "color" : "yellow"});
+            }, function(){
+             $(".b4").css({ "background" : "none","color" : "black"});
+            });
+        });
+        $(function(){
+         $(".h5").hover
+         (function()
+            {
+             $(".a5").css({"background" : "rgb(8,81,144)"});   
+            }, function() {
+             $(".a5").css({"background" : "none"});   
+            });
+        });
+        $(function(){
+         $(".h5").hover
+         (function()
+            {
+             $(".b5").css({ "background" : "rgb(8,81,144)" , "color" : "yellow"});
+            }, function(){
+             $(".b5").css({ "background" : "none","color" : "black"});
+            });
+        });
+        $(function(){
+         $(".h6").hover
+         (function()
+            {
+             $(".a6").css({"background" : "rgb(8,81,144)"});   
+            }, function() {
+             $(".a6").css({"background" : "none"});   
+            });
+        });
+        $(function(){
+         $(".h6").hover
+         (function()
+            {
+             $(".b6").css({ "background" : "rgb(8,81,144)" , "color" : "yellow"});
+            }, function(){
+             $(".b6").css({ "background" : "none","color" : "black"});
+            });
+        });
+        $(function(){
+         $(".h7").hover
+         (function()
+            {
+             $(".a7").css({"background" : "rgb(8,81,144)"});   
+            }, function() {
+             $(".a7").css({"background" : "none"});   
+            });
+        });
+        $(function(){
+         $(".h7").hover
+         (function()
+            {
+             $(".b7").css({ "background" : "rgb(8,81,144)" , "color" : "yellow"});
+            }, function(){
+             $(".b7").css({ "background" : "none","color" : "black"});
+            });
+        });
+        $(function(){
+         $(".h8").hover
+         (function()
+            {
+             $(".a8").css({"background" : "rgb(8,81,144)"});   
+            }, function() {
+             $(".a8").css({"background" : "none"});   
+            });
+        });
+        $(function(){
+         $(".h8").hover
+         (function()
+            {
+             $(".b8").css({ "background" : "rgb(8,81,144)" , "color" : "yellow"});
+            }, function(){
+             $(".b8").css({ "background" : "none","color" : "black"});
+            });
+        });
+ 
+        //https://necronomicon-release.netlify.app/
+        function music_refresh(obj) {
+     document.querySelector("iframe").src = `https://www.youtube.com/embed/${obj.attributes[2].nodeValue}`
+     console.clear()
+ }
