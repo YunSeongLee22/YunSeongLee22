eb601821a930b6162a759eb607104f0d852c4347
YunSeongLee22 <91048293+YunSeongLee22@users.noreply.github.com>
2021-09-25 22:59:17 +0900

    Update kakaomap.js

--- kakaomap.js
+++ kakaomap.js
@@ -1,6 +1,4 @@
-<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=1b8fc4579a1a928991b2e5589e1c8f2d
-"></script>
-	
+
 		var container = document.getElementById('map');
 		var options = {
 			center: new kakao.maps.LatLng(33.450701, 126.570667),
