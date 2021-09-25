13c7029bcdba4a001fca0cc25ef385cc9b303e51
YunSeongLee22 <91048293+YunSeongLee22@users.noreply.github.com>
2021-09-25 23:01:41 +0900

    Update kakaomap.js

--- kakaomap.js
+++ kakaomap.js
@@ -1,4 +1,5 @@
 
+	function kakaomap(){
 		var container = document.getElementById('map');
 		var options = {
 			center: new kakao.maps.LatLng(33.450701, 126.570667),
@@ -6,4 +7,8 @@
 		};
 
 		var map = new kakao.maps.Map(container, options);
-	
+	};
+
+	$(function(){
+		kakaomap();
+	})
